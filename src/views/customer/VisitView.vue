aee5c4fcea9047e9e2d9080dcd7a95d28e2b135f
BlingblingJK <fjkzzcq@163.com>
2023-03-21 15:16:04 +0800

    add_xuniliebiao

--- src/views/customer/VisitView.vue
+++ src/views/customer/VisitView.vue
@@ -80,14 +80,21 @@ export default {
     },
     //虚拟列表
     updateVisibleData(scrollTop = 0) {
-      console.log("111", this.$refs.scrollBox.clientHeight);
       const visibleCount = Math.ceil(
         this.$refs.scrollBox.clientHeight / this.itemHeight
       );
+
       const start = Math.floor(scrollTop / this.itemHeight);
       const end = start + visibleCount;
       this.visibleData = this.tableData.slice(start, end);
-      console.log("1111", this.visibleData, start, end);
+      console.log(
+        "1111",
+        this.visibleData,
+        start,
+        end,
+        visibleCount,
+        this.$refs.scrollBox.clientHeight / this.itemHeight
+      );
       this.$refs.content.style.webkitTransform = `translate3d(0, ${
         start * this.itemHeight
       }px, 0)`;
@@ -111,4 +118,37 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style>
+.list-view {
+  height: 590px;
+  overflow: auto;
+  position: relative;
+  border: 1px solid #aaa;
+}
+
+.list-view-phantom {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: -1;
+}
+
+.list-view-content {
+  left: 0;
+  right: 0;
+  top: 0;
+  position: absolute;
+}
+
+.list-view-item {
+  padding: 5px;
+  color: #666;
+  line-height: 30px;
+  box-sizing: border-box;
+}
+
+[v-cloak] {
+  display: none;
+}
+</style>
